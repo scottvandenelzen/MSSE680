04233f0fb9fc78a2174f6871914a271568303cc8
scottvandenelzen <scott@vandenelzen.com>
2013-09-08 11:43:04 -0500

    added unit test project

---
+++ DAL/DALUnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Linq;
+using DAL;
+
+namespace DALUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // database context
+            scottEntities db = new scottEntities();
+
+            // act -- go get the first record
+            Contact savedContact = (from d in db.Contacts where d.ContactID == 1 select d).Single();
+
+            // assert
+            Assert.AreEqual(savedContact.ContactID, 1);
+
+        }
+    }
+}
---
+++ DALUnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using System.Data;
+using System.Linq;
+
+namespace DALUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            scottEntities db = new scottEntities();
+
+            Contact myContact = (from d in db.Contacts where d.ContactID == 1 select d).Single();
+
+            Assert.AreEqual(myContact.ContactID, 1);
+
+        }
+    }
+}
